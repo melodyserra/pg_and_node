66c65f9b7c75a9f2b9bca492ff61379fb7edbf32
Melody Serra <melodyserra@gmail.com>
2014-10-19 22:50:31 -0700

    solutions

--- superdry.js
+++ superdry.js
@@ -2,18 +2,18 @@
 
 var DB = require('./mydb_lib.js');
 
-var db = new DB("library_example_app", 5432, "localhost");
+var library_db = new DB("library_example_app", 5432, "localhost");
 
-db.query("SELECT * FROM books;", function(err, resultSet){
+library_db.query("SELECT * FROM books;", function(err, resultSet){
     if (err) console.log("SELECT FAILED :-(", err);
     console.log(resultSet.rows);
 });
 
-db.query("INSERT INTO books (title, author) VALUES ($1, $2) RETURNING *",
+library_db.query("INSERT INTO books (title, author) VALUES ($1, $2) RETURNING *",
 ["The Great Gatsby", "Fitzgerald"], function(err, resultSet){
   if (err) console.log("INSERT FAILED :-(", err);
   console.log(resultSet.rows);
 });
 
-db.end();
+library_db.end();
 
